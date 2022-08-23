f1abc4742792d6416a51c9906c68d49e921606bb
chromium1 <111808325+chromium1@users.noreply.github.com>
2022-08-23 16:10:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
-streamlit('
+streamlit.header('🍌🍓 Build Your Your Smoothie 🍇🥝')
