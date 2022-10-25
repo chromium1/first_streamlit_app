aec38a4e23fcf846373b5993e57430d5534f7f9a
chromium1 <111808325+chromium1@users.noreply.github.com>
2022-10-25 15:01:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,14 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
 
+# create the repeatable code block (called a function)
+def get_fruityvide_data(this_fruit_choice):
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      # streamlit.text(fruityvice_response.json()) # Just writes data to the screen
+      # Take the json version of the response and normalize it
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      return fruityvice_normalized
+
 # New section to display Fruityvice API response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
@@ -35,12 +43,14 @@ try:
   else:
       # import requests
       # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
       # streamlit.text(fruityvice_response.json()) # Just writes data to the screen
       # Take the json version of the response and normalize it
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
       # output it on the screen as a table
-      streamlit.dataframe(fruityvice_normalized)
+      # streamlit.dataframe(fruityvice_normalized)
+      back_from_function = get_fruityvice_data(fruit_choice)
+      streamlit.dataframe(back_from_function)
 
 except URLError as e:
     streamlit.error()
