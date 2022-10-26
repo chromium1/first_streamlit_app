80f323b8d5a98e4b3bf36cf29c92a84143c38899
chromium1 <111808325+chromium1@users.noreply.github.com>
2022-10-25 20:38:19 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-streamlit.title('Avaz New Healthy Diner')
+streamlit.title('Avas New Healthy Cafe')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
